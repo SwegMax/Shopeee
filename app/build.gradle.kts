3ac4d90767d147fc754ea3ffe2fb2d53ffcda454
SwegMax <pwftw.jervis@gmail.com>
2024-02-21 02:03:46 +0800

    finally fixed kotlin problem

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     id("com.android.application")
-    id("org.jetbrains.kotlin.android") version "1.8.0" apply false
+    id("org.jetbrains.kotlin.android") version "1.8.20" apply false
 }
 
 android {
@@ -41,6 +41,7 @@ dependencies {
     implementation("androidx.navigation:navigation-ui:2.6.0")
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation(platform("org.jetbrains.kotlin:kotlin-bom:1.8.20"))
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
