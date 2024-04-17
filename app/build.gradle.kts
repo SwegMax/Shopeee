6062479d85a256c555f56f24dc8f7c59ffe5b31d
SwegMax <pwftw.jervis@gmail.com>
2024-04-17 23:05:09 +0800

    bottom bar

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -62,6 +62,8 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    implementation("com.google.android.material:material:1.11.0")
+
 
     //ViewModel compose
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.4.1")
