f5dc34101b16a42ab57025282967a4ddae5d60f4
SwegMax <pwftw.jervis@gmail.com>
2024-08-06 00:33:30 +0800

    merge conflict resolved

--- app/src/main/java/com/example/shopeee/views/IntroFragment.kt
+++ app/src/main/java/com/example/shopeee/views/IntroFragment.kt
@@ -11,7 +11,6 @@ class IntroFragment : Fragment() {
     /*lateinit var app : App
     private val appId = "shopeee-zuqyq"*/
     private lateinit var binding: FragmentIntroductionBinding
-<<<<<<< HEAD:app/src/main/java/com/example/shopeee/views/FirstFragment.kt
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -24,19 +23,5 @@ class IntroFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
     }
-=======
-        override fun onCreateView(
-                inflater: LayoutInflater,
-                container: ViewGroup?,
-                savedInstanceState: Bundle?
-        ): View? {
-            binding = FragmentIntroductionBinding.inflate(layoutInflater)
-            return binding.root
-        }
-
-        override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-            super.onViewCreated(view, savedInstanceState)
-        }
->>>>>>> 3fe3923b2cedaf376d9dbc543a8909dc303f6304:app/src/main/java/com/example/shopeee/views/IntroFragment.kt
 
 }
