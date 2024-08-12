8e713d38995da4e6c0e87912300064289bad2936
SwegMax <pwftw.jervis@gmail.com>
2024-08-12 23:27:33 +0800

    blank page

--- app/src/main/java/com/example/shopeee/views/LoginRegisterActivity.kt
+++ app/src/main/java/com/example/shopeee/views/LoginRegisterActivity.kt
@@ -1,10 +1,12 @@
 package com.example.shopeee.views
 
-import android.app.Activity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.example.shopeee.R
+import dagger.hilt.android.AndroidEntryPoint
 
-class LoginRegisterActivity : Activity() {
+@AndroidEntryPoint
+class LoginRegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login_register)
