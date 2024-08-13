3fcf7b7035b2f368b0e0d4182e4bf46fa6d3b680
Jervis <44323554+SwegMax@users.noreply.github.com>
2024-08-13 12:50:51 +0800

    Update LoginRegisterActivity.kt
    Testing remote commits

--- app/src/main/java/com/example/shopeee/views/LoginRegisterActivity.kt
+++ app/src/main/java/com/example/shopeee/views/LoginRegisterActivity.kt
@@ -10,5 +10,7 @@ class LoginRegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login_register)
+
+        Log.d(TAG, "LoginRegisterActivity loaded")
     }
 }
