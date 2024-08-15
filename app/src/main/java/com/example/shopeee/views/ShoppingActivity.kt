5d9fbe7ef6049c303b380d693b54f120324cb531
SwegMax <pwftw.jervis@gmail.com>
2024-08-16 01:29:11 +0800

    fix fix

--- app/src/main/java/com/example/shopeee/views/ShoppingActivity.kt
+++ app/src/main/java/com/example/shopeee/views/ShoppingActivity.kt
@@ -2,12 +2,16 @@ package com.example.shopeee.views
 
 import android.app.Activity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.Navigation
 import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import androidx.navigation.ui.setupWithNavController
 import com.example.shopeee.R
 import com.example.shopeee.databinding.ActivityShoppingBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
-class ShoppingActivity : Activity() {
+class ShoppingActivity : AppCompatActivity() {
 
     val binding by lazy {
         ActivityShoppingBinding.inflate(layoutInflater)
@@ -16,7 +20,11 @@ class ShoppingActivity : Activity() {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
-        val navController = findNavController(R.id.shoppingHostFragment)
-        binding.bottomNavigation.setupWithNavController(navController)
+        /*val navController = findNavController(R.id.shoppingHostFragment)
+        binding.bottomNavigation.setupWithNavController(navController)*/
+
+        val bottomNavigation = findViewById<BottomNavigationView>(R.id.bottomNavigation)
+        val navController = Navigation.findNavController(this, R.id.shoppingHostFragment)
+        NavigationUI.setupWithNavController(bottomNavigation, navController)
     }
 }
