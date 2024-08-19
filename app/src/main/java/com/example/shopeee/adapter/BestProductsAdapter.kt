ee88f4c168bae778224ca3c6ac565650d31b50f9
Jervis <44323554+SwegMax@users.noreply.github.com>
2024-08-19 15:29:47 +0800

    Update BestProductsAdapter.kt
    replace with S empty images

--- app/src/main/java/com/example/shopeee/adapter/BestProductsAdapter.kt
+++ app/src/main/java/com/example/shopeee/adapter/BestProductsAdapter.kt
@@ -8,6 +8,7 @@ import androidx.recyclerview.widget.AsyncListDiffer
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.example.shopeee.R
 import com.example.shopeee.databinding.ProductRvItemBinding
 import com.example.shopeee.repository.Product
 
@@ -16,17 +17,21 @@ class BestProductsAdapter: RecyclerView.Adapter<BestProductsAdapter.BestProducts
     inner class BestProductsViewHolder(private val binding: ProductRvItemBinding): RecyclerView.ViewHolder(binding.root) {
         fun bind(product: Product) { //i think product was initialised in the previous
             binding.apply {
-                Glide.with(itemView).load(product.images[0]).into(imgProduct)
-                product.offerPercentage?.let{
-                    val remainingPricePercentage = 1f - it
-                    val priceAfterOffer = remainingPricePercentage * product.price
-                    tvNewPrice.text = "$ ${String.format("%.2f", priceAfterOffer)}"
-                    tvPrice.paintFlags = Paint.STRIKE_THRU_TEXT_FLAG
+                if (product.images.isNotEmpty()) {
+                    Glide.with(itemView).load(product.images[0]).into(imgProduct)
+                    product.offerPercentage?.let{
+                        val remainingPricePercentage = 1f - it
+                        val priceAfterOffer = remainingPricePercentage * product.price
+                        tvNewPrice.text = "$ ${String.format("%.2f", priceAfterOffer)}"
+                        tvPrice.paintFlags = Paint.STRIKE_THRU_TEXT_FLAG
+                    }
+                    if (product.offerPercentage == null)
+                        tvNewPrice.visibility = View.INVISIBLE
+                    tvPrice.text = "$ ${product.price}"
+                    tvName.text = product.name
+                } else {
+                    imgProduct.setImageResource(R.drawable.alpha_s_box)
                 }
-                if (product.offerPercentage == null)
-                    tvNewPrice.visibility = View.INVISIBLE
-                tvPrice.text = "$ ${product.price}"
-                tvName.text = product.name
             }
         }
     }
