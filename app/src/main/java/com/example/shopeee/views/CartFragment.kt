0f07bf231c687d34ef7d509a52944df54ace8756
Jervis <44323554+SwegMax@users.noreply.github.com>
2024-08-19 17:17:46 +0800

    Update CartFragment.kt
    3

--- app/src/main/java/com/example/shopeee/views/CartFragment.kt
+++ app/src/main/java/com/example/shopeee/views/CartFragment.kt
@@ -1,7 +1,19 @@
 package com.example.shopeee.views
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.shopeee.R
 
 class CartFragment : Fragment(R.layout.fragment_cart) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_cart, container, false)
+    }
 }
