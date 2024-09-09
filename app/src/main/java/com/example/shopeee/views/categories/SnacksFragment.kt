3d3523d15d40c39c832aaae3b0056610f06f4ee3
Jervis <44323554+SwegMax@users.noreply.github.com>
2024-09-10 01:00:56 +0800

    Update SnacksFragment.kt
    replaced Started with repeatWhenstarted

--- app/src/main/java/com/example/shopeee/views/categories/SnacksFragment.kt
+++ app/src/main/java/com/example/shopeee/views/categories/SnacksFragment.kt
@@ -3,7 +3,9 @@ package com.example.shopeee.views.categories
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import com.example.shopeee.repository.Category
 import com.example.shopeee.repository.Resource
 import com.example.shopeee.viewmodelMVVM.CategoryViewModel
@@ -12,6 +14,7 @@ import com.google.android.material.snackbar.Snackbar
 import com.google.firebase.firestore.FirebaseFirestore
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @AndroidEntryPoint
@@ -26,42 +29,60 @@ class SnacksFragment: BaseCategoryFragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        lifecycleScope.launchWhenStarted {
-            viewModel.offerProducts.collectLatest {
-                when(it) {
-                    is Resource.Loading -> {
-                        showOfferLoading()
+        lifecycleScope.launch {
+            viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
+                viewModel.offerProducts.collectLatest {
+                    when (it) {
+                        is Resource.Loading -> {
+                            showOfferLoading()
+                        }
+
+                        is Resource.Success -> {
+                            offerAdapter.differ.submitList(it.data)
+                            hideOfferLoading()
+                        }
+
+                        is Resource.Error -> {
+                            Snackbar.make(
+                                requireView(),
+                                "snackOfferProd error",
+                                Snackbar.LENGTH_LONG
+                            )
+                                .show()
+                            hideOfferLoading()
+                        }
+
+                        else -> Unit
                     }
-                    is Resource.Success -> {
-                        offerAdapter.differ.submitList(it.data)
-                        hideOfferLoading()
-                    }
-                    is Resource.Error -> {
-                        Snackbar.make(requireView(), "snackOfferProd error", Snackbar.LENGTH_LONG)
-                            .show()
-                        hideOfferLoading()
-                    }
-                    else -> Unit
                 }
             }
         }
 
-        lifecycleScope.launchWhenStarted {
-            viewModel.bestProducts.collectLatest {
-                when(it) {
-                    is Resource.Loading -> {
-                        showBestProductsLoading()
+        lifecycleScope.launch {
+            viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
+                viewModel.bestProducts.collectLatest {
+                    when (it) {
+                        is Resource.Loading -> {
+                            showBestProductsLoading()
+                        }
+
+                        is Resource.Success -> {
+                            bestProductsAdapter.differ.submitList(it.data)
+                            hideBestProductsLoading()
+                        }
+
+                        is Resource.Error -> {
+                            Snackbar.make(
+                                requireView(),
+                                "it.message.toString()",
+                                Snackbar.LENGTH_LONG
+                            )
+                                .show()
+                            hideBestProductsLoading()
+                        }
+
+                        else -> Unit
                     }
-                    is Resource.Success -> {
-                        bestProductsAdapter.differ.submitList(it.data)
-                        hideBestProductsLoading()
-                    }
-                    is Resource.Error -> {
-                        Snackbar.make(requireView(), "it.message.toString()", Snackbar.LENGTH_LONG)
-                            .show()
-                        hideBestProductsLoading()
-                    }
-                    else -> Unit
                 }
             }
         }
