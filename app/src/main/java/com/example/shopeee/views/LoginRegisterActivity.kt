4d4bb690717af3a604961c8b895e5e3722a90c94
Jervis <44323554+SwegMax@users.noreply.github.com>
2024-09-10 00:54:27 +0800

    Update LoginRegisterActivity.kt
    Login check logic here

--- app/src/main/java/com/example/shopeee/views/LoginRegisterActivity.kt
+++ app/src/main/java/com/example/shopeee/views/LoginRegisterActivity.kt
@@ -4,45 +4,27 @@ import android.content.ContentValues.TAG
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
-import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.lifecycleScope
 import com.example.shopeee.R
-import com.example.shopeee.repository.Resource
-import com.example.shopeee.viewmodelMVVM.LoginViewModel
+import com.google.firebase.auth.FirebaseAuth
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class LoginRegisterActivity : AppCompatActivity() {
-
-    private val viewModel: LoginViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login_register)
-        observeLoginState()
+
+        val currentUser = FirebaseAuth.getInstance().currentUser
+        if (currentUser != null) {
+            startActivity(Intent(this, ShoppingActivity::class.java))
+            finish()
+        } else {
+            setContentView(R.layout.activity_login_register)
+        }
         Log.d(TAG, "LoginRegisterActivity loaded")
     }
-    private fun observeLoginState() {
-        lifecycleScope.launch {
-            viewModel.login.collect { resource ->
-                when (resource) {
-                    is Resource.Success -> {
-                        if (resource.data == null) {
-                            // User is already logged in, navigate to ShoppingActivity
-                            val intent = Intent(this@LoginRegisterActivity, ShoppingActivity::class.java)
-                            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK)
-                            startActivity(intent)
-                            finish()
-                        } else {
-                        }
-                    }
-                    is Resource.Error -> {
-                    }
-                    else -> {
-                    }
-                }
-            }
-        }
+
+    override fun onBackPressed() {
+        finishAffinity()
     }
 }
