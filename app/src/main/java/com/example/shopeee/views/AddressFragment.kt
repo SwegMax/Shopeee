41f9923b02277d959a362a0482ac85f610664440
SwegMax <pwftw.jervis@gmail.com>
2025-06-27 15:56:19 +0800

    Address and Billing

---
+++ app/src/main/java/com/example/shopeee/views/AddressFragment.kt
@@ -0,0 +1,80 @@
+package com.example.shopeee.views
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.findNavController
+import com.example.shopeee.databinding.FragmentAddressBinding
+import com.example.shopeee.repository.Address
+import com.example.shopeee.repository.Resource
+import com.example.shopeee.viewmodelMVVM.AddressViewModel
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
+
+@AndroidEntryPoint
+class AddressFragment: Fragment() {
+    private lateinit var binding: FragmentAddressBinding
+    val viewModel by viewModels<AddressViewModel>()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        lifecycleScope.launchWhenStarted {
+            viewModel.addNewAddress.collectLatest {
+                when(it) {
+                    is Resource.Loading -> {
+                        binding.progressbarAddress.visibility = View.VISIBLE
+                    }
+                    is Resource.Success -> {
+                        binding.progressbarAddress.visibility = View.INVISIBLE
+                        findNavController().navigateUp()
+                    }
+                    is Resource.Error -> {
+                        Toast.makeText(requireContext(), it.message, Toast.LENGTH_SHORT).show()
+                    }
+                    else -> Unit
+                }
+            }
+        }
+
+        lifecycleScope.launch {
+            viewModel.error.collectLatest {
+                Toast.makeText(requireContext(), it, Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentAddressBinding.inflate(inflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.apply {
+            buttonSave.setOnClickListener {
+                val addressTitle = edAddressTitle.text.toString()
+                val fullName = edFullName.text.toString()
+                val street = edStreet.text.toString()
+                val phone = edPhone.text.toString()
+                val city = edCity.text.toString()
+                val address = Address(addressTitle, fullName, street, phone, city)
+
+                viewModel.addAddress(address)
+            }
+        }
+
+
+    }
+}
---
+++ app/src/main/java/com/example/shopeee/views/ProfileFragment.kt
@@ -0,0 +1,22 @@
+package com.example.shopeee.views
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.shopeee.databinding.FragmentProfileBinding
+
+class ProfileFragment: Fragment() {
+    private lateinit var binding: FragmentProfileBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentProfileBinding.inflate(inflater)
+        return binding.root
+    }
+
+}
